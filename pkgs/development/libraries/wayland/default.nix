76eb44ceef236082d2cff9f523187878e448da4a
dcol <dcol@kth.se>
2018-12-12 15:21:38 +0100

    fixes

--- pkgs/development/libraries/wayland/default.nix
+++ pkgs/development/libraries/wayland/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, pkgconfig
+{ lib, stdenv, fetchurl, pkgconfig, buildPackages, hostPlatform, buildPlatform
 , libffi, libxml2
 , expat ? null # Build wayland-scanner (currently cannot be disabled as of 1.7.0)
 }:
@@ -9,14 +9,14 @@ assert expat != null;
 stdenv.mkDerivation rec {
   name = "wayland-${version}";
   version = "1.16.0";
-
+ 
   src = fetchurl {
     url = "https://wayland.freedesktop.org/releases/${name}.tar.xz";
     sha256 = "1xajhxad43izq9f7sbww1hlg42nayijy8xnp21kgpk09c6sw4wjf";
   };
 
-  configureFlags = [ "--with-scanner" "--disable-documentation" ];
-
+configureFlags = [ "--disable-documentation" ] ++ lib.optional (hostPlatform != buildPlatform) "--with-host-scanner";
+  depsBuildBuild = [] ++ lib.optional ( hostPlatform != buildPlatform ) buildPackages.wayland;
   nativeBuildInputs = [ pkgconfig ];
 
   buildInputs = [ libffi /* docbook_xsl doxygen graphviz libxslt xmlto */ expat libxml2 ];
